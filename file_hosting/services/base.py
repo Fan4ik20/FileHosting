2e7c37ad977c84c77038795585f9c35bbaede827
Mark Zaianchkovskyi <marko4148@gmail.com>
2022-10-15 02:03:14 +0300

    Added base class for services

---
+++ file_hosting/services/base.py
@@ -0,0 +1,22 @@
+from typing import TypeVar, Generic, Iterable
+
+
+Repository = TypeVar('Repository')
+Repr = TypeVar('Repr')
+
+
+class ServiceBase(Generic[Repository, Repr]):
+    def __init__(self, repository: Repository) -> None:
+        self.repository = repository
+
+    def get(self, *args) -> Repr:
+        raise NotImplementedError
+
+    def get_all(self, *args) -> Iterable[Repr]:
+        raise NotImplementedError
+
+    def create(self, *args) -> Repr:
+        raise NotImplementedError
+
+    def delete(self, *args) -> None:
+        raise NotImplementedError
