9a306e910dc07bdb75b9adfbcf362655f2d71d65
Mark Zaianchkovskyi <marko4148@gmail.com>
2022-10-18 00:46:45 +0300

    Added user routes

--- file_hosting/api/routers/hosting_router.py
+++ file_hosting/api/routers/hosting_router.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from . import directories
 from . import auth
+from . import user
 
 
 router = APIRouter()
 
 router.include_router(directories.router)
 router.include_router(auth.router)
+router.include_router(user.router)
---
+++ file_hosting/api/routers/user.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import Response
+
+from services.abstract.user_base import AUserService, UserRepr
+from services.exceptions import base as service_exc
+
+from api.dependencies.stubs.services import UserServiceS
+from api.dependencies.stubs.auth import ActiveUserS
+
+from api.exceptions import exc as http_exc
+
+from .schemas.user import UserGet
+
+
+router = APIRouter(prefix='/users', tags=['Users'])
+
+
+@router.get('/me/', response_model=UserGet)
+def get_active_user(user: UserRepr = Depends(ActiveUserS)):
+    return user
+
+
+@router.delete(
+    '/me/', response_class=Response, status_code=status.HTTP_204_NO_CONTENT
+)
+def delete_active_user(
+        user: UserRepr = Depends(ActiveUserS),
+        user_service: AUserService = Depends(UserServiceS)
+):
+    # FIXME.
+
+    try:
+        user_service.delete(user.id)
+    except service_exc.NotFoundError as err:
+        raise http_exc.ObjectNotExistInPath(err.model)
